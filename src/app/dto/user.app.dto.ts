b8d21b42e6bdf038ddb49bab0f5f88a5baf9b74b
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-07-15 18:05:35 -0300

    Created folder app with dto to manager input and output data

---
+++ src/app/dto/user.app.dto.ts
@@ -0,0 +1,7 @@
+import { UserEntity } from 'src/core/entities/user.entity'
+
+export type UserAppInPutDto = UserEntity
+export type UserAppOutPutDto = UserEntity
+export type UserAppCreatedOutPutDto = {
+  id: string
+}
