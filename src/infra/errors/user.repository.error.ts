de1d2298f0932856fbf4cab3fd38102cf4579b1a
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-07-15 18:09:32 -0300

    Moved folder error

--- src/infra/errors/user.repository.error.ts
+++
@@ -1,6 +0,0 @@
-export class UserRepositoryFailAuth extends Error {
-  constructor() {
-    super()
-    this.message = 'Fail to authenticate the user, verify the email or password'
-  }
-}
--- src/infra/port/user.repository.port.ts
+++ src/infra/port/user.repository.port.ts
@@ -1,7 +1,10 @@
 import { UserAppCreatedOutPutDto } from 'src/app/dto'
 import { Either } from 'src/shared/error/either'
+import { UserRepositoryFailAuth } from '../repositories/errors'
 
 export interface UserRepositoryPort {
   create: () => Promise<Either<Error, UserAppCreatedOutPutDto>>
-  validate: () => Promise<Either<Error, UserAppCreatedOutPutDto>>
+  validate: () => Promise<
+    Either<UserRepositoryFailAuth, UserAppCreatedOutPutDto>
+  >
 }
