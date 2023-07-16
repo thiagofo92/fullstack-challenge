03a60678c6393b6707b8f13ba45e761f23bca031
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-07-16 00:07:00 -0300

    Add input type to dto

--- src/app/dto/archive-task.app.dto.ts
+++ src/app/dto/archive-task.app.dto.ts
@@ -1,2 +1,3 @@
 import { ArchiveTaskEntity } from 'src/core/entities/archive-task.entity'
 export type ArchiveTaskAppOutputDto = Array<ArchiveTaskEntity>
+export type ArchiveTaskAppInputDto = ArchiveTaskEntity
