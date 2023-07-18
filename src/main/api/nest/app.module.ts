7a29c0b24439cc7287b28b533b3702432afd5e20
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-07-18 08:34:46 -0300

    Add middleware helmet and rate limit

--- src/main/api/nest/app.module.ts
+++ src/main/api/nest/app.module.ts
@@ -4,9 +4,26 @@ import { UsersModule } from './controllers/users/users.module'
 import { TasksModule } from './controllers/tasks/tasks.module'
 import { ArchiveTaskModule } from './controllers/archive-task/archive-task.module'
 import { ConfigModule } from '@nestjs/config'
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler'
+import { APP_GUARD } from '@nestjs/core/constants'
 
 @Module({
-  imports: [UsersModule, TasksModule, ArchiveTaskModule, ConfigModule.forRoot()],
+  imports: [
+    UsersModule,
+    TasksModule,
+    ArchiveTaskModule,
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 15,
+    }),
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
--- src/main/api/nest/main.ts
+++ src/main/api/nest/main.ts
@@ -5,36 +5,40 @@ import { ValidationPipe } from '@nestjs/common'
 import { WinstonOptions } from '../../../shared/logger/logger'
 import { WinstonModule } from 'nest-winston'
 import { Connection } from '../../../infra/repositories/mongoose/connection/connection'
+import helmet from 'helmet'
 
 const logger = WinstonModule.createLogger(WinstonOptions)
+
 export async function bootstrap() {
   otelSDK.start()
+
   const app = await NestFactory.create(AppModule, {
     logger,
   })
 
+  app.use(helmet())
   app.useGlobalPipes(new ValidationPipe())
 
-  // process.on('SIGTERM', onSigterm)
+  process.on('SIGTERM', onSigterm)
 
   await app.listen(3000)
 }
 
-// function onSigterm() {
-//   logger.warn('Server was shutdonw')
+function onSigterm() {
+  logger.warn('Server was shutdonw')
 
-//   Promise.allSettled([Connection.closeConnection, otelSDK.shutdown])
-//     .then(([connection, otl]) => {
-//       if (connection.status == 'rejected') logger.error('Databse connection shutdown fail')
-//       else logger.log('Success to close database connection')
+  Promise.allSettled([Connection.closeConnection, otelSDK.shutdown])
+    .then(([connection, otl]) => {
+      if (connection.status == 'rejected') logger.error('Databse connection shutdown fail')
+      else logger.log('Success to close database connection')
 
-//       if (otl.status === 'rejected') logger.error('Failt o shutdown the Open telemetry')
-//       else logger.log('Success to shuwtdown open telemetry')
-//     })
-//     .catch((error) => {
-//       logger.error(error)
-//     })
-//     .finally(() => {
-//       process.exit(0)
-//     })
-// }
+      if (otl.status === 'rejected') logger.error('Failt o shutdown the Open telemetry')
+      else logger.log('Success to shuwtdown open telemetry')
+    })
+    .catch((error) => {
+      logger.error(error)
+    })
+    .finally(() => {
+      process.exit(0)
+    })
+}
