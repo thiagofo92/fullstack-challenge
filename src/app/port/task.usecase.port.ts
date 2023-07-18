08124d2bef991f916449311c916af66736e4c11b
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-07-18 12:14:02 -0300

    Add return type error

--- src/app/port/task.usecase.port.ts
+++ src/app/port/task.usecase.port.ts
@@ -1,10 +1,11 @@
 import { Either } from 'src/shared/error/either'
 import { TaskAppDtoInput, TaskAppDtoOutput, TaskAppListDtoOutput, TaskCreateAppDtoOutput } from '../dto'
+import { IdNotFound } from 'src/shared/error/not-found.error'
 
 export abstract class TaskUseCasePort {
   create: (input: TaskAppDtoInput) => Promise<Either<Error, TaskCreateAppDtoOutput>>
   filter: (idUser: string, status: string) => Promise<Either<Error, TaskAppListDtoOutput>>
   findByUserId: (idUser: string) => Promise<Either<Error, TaskAppListDtoOutput | null>>
-  delete: (id: string) => Promise<Either<Error, TaskAppDtoOutput>>
-  update: (input: TaskAppDtoInput) => Promise<Either<Error, boolean>>
+  delete: (id: string) => Promise<Either<Error | IdNotFound, TaskAppDtoOutput>>
+  update: (input: TaskAppDtoInput) => Promise<Either<Error | IdNotFound, boolean>>
 }
